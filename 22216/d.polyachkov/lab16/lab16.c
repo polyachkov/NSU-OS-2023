726aca2dac641d2195293fc03b4b31ba2ddb9d27
Dmitriy Polyachkov <d.polyachkov@g.nsu.ru>
2024-08-31 15:43:36 +0700

    lab16 create

---
+++ 22216/d.polyachkov/lab16/lab16.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+void set_terminal_mode() {
+    struct termios new_settings;
+    if (tcgetattr(STDIN_FILENO, &new_settings) != 0) {
+        perror("Error getting terminal attributes");
+        exit(-1);
+    }
+    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= ~ECHO;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &new_settings) != 0) {
+        perror("Error setting terminal attributes");
+        exit(-1);
+    }
+}
+
+void reset_terminal_mode(struct termios *original_settings) {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, original_settings) != 0) {
+        perror("Error restoring terminal attributes");
+        exit(-1);
+    }
+}
+
+int main() {
+    struct termios original_settings;
+    if (tcgetattr(STDIN_FILENO, &original_settings) != 0) {
+        perror("Error getting terminal attributes");
+        return -1;
+    }
+    set_terminal_mode();
+
+    printf("Continue? (y/n): ");
+    fflush(stdout);
+
+    char answer = getchar();
+    if (answer == EOF) {
+        perror("Error reading character");
+        reset_terminal_mode(&original_settings);
+        return -1;
+    }
+
+    reset_terminal_mode(&original_settings);
+
+    printf("\n");
+
+    if (answer == 'y' || answer == 'Y') {
+        printf("You chose to continue.\n");
+    } else if (answer == 'n' || answer == 'N') {
+        printf("You chose to stop.\n");
+    } else {
+        printf("Invalid choice.\n");
+    }
+
+    return 0;
+}
