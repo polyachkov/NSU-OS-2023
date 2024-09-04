bd424460068c4be6412f07d1902fe0840ae54adc
polyachkov <144012933+polyachkov@users.noreply.github.com>
2024-09-05 02:25:12 +0700

    Update lab5.c

--- 22216/d.polyachkov/lab5/lab5.c
+++ 22216/d.polyachkov/lab5/lab5.c
@@ -119,7 +119,7 @@ int main(int argc, char const *argv[]) {
         }
         if (sc != 1 || findLine < 0 || findLine > table.size) {
             fprintf(stderr, "Incorrect line number. Line should be a number from 1 to %d.\n", table.size);
-            int ch;
+            char ch;
             while ((ch = getchar()) != '\n' && ch != EOF);
             continue;
         }
