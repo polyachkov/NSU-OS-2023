5d74c5fe2974dfc3c0a15deb6c44e0fb2b0066e5
polyachkov <144012933+polyachkov@users.noreply.github.com>
2024-09-14 04:27:01 +0700

    Create lab9.c

---
+++ 22216/d.polyachkov/lab9/lab9.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(1);
+    }
+
+    if (access(argv[1], F_OK) == -1) {
+        perror("File access check failed");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        char *args[] = {"cat", argv[1], NULL};
+        execvp("cat", args);
+        perror("execvp failed");
+        exit(1);
+    }
+
+    time_t start = time(NULL);
+    if(start == (time_t) -1) {
+        perror("time error");
+        exit(1);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid failed");
+        exit(1);
+    }
+
+    time_t end = time(NULL);
+    if(end == (time_t) -1) {
+        perror("time error");
+        exit(1);
+    }
+
+    printf("Parent waited %ld seconds for child to complete.\n", end - start);
+
+    if (WIFEXITED(status)) {
+        printf("Child exited normally with status: %d\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        printf("Child was terminated by signal: %d\n", WTERMSIG(status));
+    }
+
+    return 0;
+}
